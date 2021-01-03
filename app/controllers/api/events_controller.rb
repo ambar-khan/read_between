026c80db68cfb9009961d560e1ad4255586811ed
Ambar <ambarmogg@me.com>
2021-01-03 08:56:34 -0800

    add image column to events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -9,6 +9,26 @@ class Api::EventsController < ApplicationController
     the_id = params[:id]
     @event = Event.find_by(id: the_id)
     render 'show.json.jb'
+
+
+    # Autopopulate Book Info
+
+    # response = HTTP.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{params[:isbn13]}&key=#{Rails.application.credentials.google_books_api[:api_key]}")
+
+    # book = response.parse
+    
+    # # book_id = book["items"][0]["volumeInfo"]["industryIdentifiers"][1]["identifier"]
+  
+    # book_title = book["items"][0]["volumeInfo"]["title"]
+
+    # book_subtitle = book["items"][0]["volumeInfo"]["subtitle"]
+    
+    # book_author = book["items"][0]["volumeInfo"]["authors"]
+
+    # book_description = book["items"][0]["volumeInfo"]["description"]
+
+    # book_image = book["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
+
   end
 
   def create
@@ -16,10 +36,20 @@ class Api::EventsController < ApplicationController
     name: params[:name],
     date: params[:date], 
     time: params[:time],
-    meeting_link: params[:meeting_link]
+    meeting_link: params[:meeting_link],
+    # book_id: params[:isbn13]
     )
     @event.save
     render 'show.json.jb'
   end
 
 end
+
+ 
+
+    # .then(response => response.json())
+    # .then(result => {
+    # this.setState({ books: result.items})
+    # })
+
+#{params[:isbn_13]}
