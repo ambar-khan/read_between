b5b0886ecbac1fb3c790b46e7c9d91d58d7a78e2
Ambar <ambarmogg@me.com>
2021-01-18 21:27:46 -0800

    change validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_many :event_registrations
   has_many :events, through: :event_registrations
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  # validates :email, presence: true, uniqueness: true
 end
