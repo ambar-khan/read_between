910288de2e991a2ab99ff4b40337e9a769a74c22
Ambar <ambarmogg@me.com>
2021-01-19 11:04:45 -0800

    add assets

---
+++ public/assets/js/js.jquery.comments.js
@@ -0,0 +1,15 @@
+$('#comments-container').comments({
+  profilePictureURL: 'https://viima-app.s3.amazonaws.com/media/public/defaults/user-icon.png',
+  getComments: function (success, error) {
+    var commentsArray = [{
+      id: 1,
+      created: '2015-10-01',
+      content: 'Lorem ipsum dolort sit amet',
+      fullname: 'Simon Powell',
+      upvote_count: 2,
+      user_has_upvoted: false
+    }];
+    success(commentsArray);
+  }
+});
+
